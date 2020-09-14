8e3a4bb2ccb555ee71cb638d66e1db75e5020676
Natalia Denisenko <natdeni@google.com>
2020-09-14 10:41:41 +0000

    Resolve errors

--- src/main/java/com/google/sps/servlets/ProfileImage.java
+++ src/main/java/com/google/sps/servlets/ProfileImage.java
@@ -40,8 +40,10 @@ public class ProfileImage extends HttpServlet {
       Key userKey = KeyFactory.createKey("User", userId);
       Entity userEntity = datastore.get(userKey);
       String fileName = (String) userEntity.getProperty("avatarName");
-      if (fileName == "default.png") {
+      if (fileName.equals("default.png")) {
+        fileName = userId + ".png";
         userEntity.setProperty("avatarName", userId + ".png");
+        datastore.put(userEntity);
       }
 
       Part filePart = request.getPart("avatar");
